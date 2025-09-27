c64e10d1365caacf74ae2d1f165219a823f0cfa8
westbrookcook <pvjkdkw0818@hotmail.com>
2025-09-27 12:29:34 +0800

    delete useless ui

--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
 
 import { config } from './config/wagmi';
-import App from './App.tsx';
-
-import '@rainbow-me/rainbowkit/styles.css';
-import './index.css';
+import { VoteApp } from './components/VoteApp';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+function App() {
+  return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
+        <RainbowKitProvider locale="en">
+          <div style={{ minHeight: '100vh', backgroundColor: '#f9fafb' }}>
+            <VoteApp />
+          </div>
         </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
-  </StrictMode>,
-)
+  );
+}
+
+export default App
